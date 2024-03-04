2211a5f743415c7399562f1fd148139a7f3d5260
marco-tuzza <marcotuzza@hotmail.it>
2024-03-04 21:07:18 +0100

    Created a simple analog clock with hours, minutes and seconds

---
+++ src/Clock.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+export default function Clock() {
+    const [degrees, setDegrees] = useState({ hours: 0, minutes: 0, seconds: 0 });
+
+    function calculateDegrees() {
+        const date = new Date();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        const degreesMinutes = (minutes * 360) / 60;
+        const degreesHours = (hours * 360) / 12 + (minutes * 360) / 60 / 12;
+        const degreesSeconds = (seconds * 360) / 60;
+
+        setDegrees({
+            hours: degreesHours,
+            minutes: degreesMinutes,
+            seconds: degreesSeconds,
+        });
+    }
+
+    useEffect(() => {
+        const interval = setInterval(calculateDegrees, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    var styleMinutes = { transform: `translate(-50%, -100%) rotate(${degrees.minutes}deg)` }
+    var styleHours = { transform: `translate(-50%, -100%) rotate(${degrees.hours}deg)` }
+    var styleSeconds = { transform: `translate(-50%, -100%) rotate(${degrees.seconds}deg)` }
+
+    return (
+        <div className="dial">
+            <div className="pin"></div>
+            <div className="hour-hand" style={styleHours}></div>
+            <div className="minute-hand" style={styleMinutes}></div>
+            <div className="seconds-hand" style={styleSeconds}></div>
+        </div>
+    );
+}
