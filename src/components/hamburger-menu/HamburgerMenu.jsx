c5746ecb7ea0eecf848679a69272f720851770ce
Marco Tuzza <marcotuzza@hotmail.it>
2024-03-15 03:03:04 +0400

    Added basic menu for changing project

--- src/components/hamburger-menu/HamburgerMenu.jsx
+++ src/components/hamburger-menu/HamburgerMenu.jsx
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import './HamburgerMenu.css';
 
-export default function HamburgerMenu() {
-    return (
-        <div className="hamburger-menu">
-            <div className="hamburger-menu__line"></div>
-            <div className="hamburger-menu__line"></div>
-            <div className="hamburger-menu__line"></div>
+export default function HamburgerMenu({ onChangeProject }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="hamburger-menu__line"></div>
+        <div className="hamburger-menu__line"></div>
+        <div className="hamburger-menu__line"></div>
+      </div>
+      {isOpen && (
+        <div className="menu">
+          <button onClick={() => onChangeProject('clock')}>Orologio</button>
+          <button onClick={() => onChangeProject('todo')}>To-Do List</button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
